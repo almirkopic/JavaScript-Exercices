3233b10c8e22d2498fd02448b706d1b994ee423b
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-11-27 23:09:41 +0100

    Update script.js

--- Exercise 27 - IIFE _ Closures/script.js
+++ Exercise 27 - IIFE _ Closures/script.js
@@ -40,3 +40,44 @@ booker(); //2
 booker(); //3
 
 console.dir(booker);
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23; // Assign a value to 'a'
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Reassigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`); // Fix typo in log message
+};
+
+const groupSize = 1000; // Change 'perGroup' to 'groupSize'
+boardPassengers(180, 3);
