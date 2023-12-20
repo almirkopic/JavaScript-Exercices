688a87e75478c2ffd0ff375c481ed665f62b4249
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-12-20 15:16:35 +0100

    Update script.js

--- Exercise 36-  Styles Attributes and Classes preparing for use in bankist website/script.js
+++ Exercise 36-  Styles Attributes and Classes preparing for use in bankist website/script.js
@@ -49,3 +49,35 @@ logo.classList.contains("c"); //not includes
 //do not use
 
 logo.className = "Almir"; // this will overwrite all existing classes it allowes us to put only one class into element
+
+// event propagation
+
+// rgb(255,255,255)
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+  //stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+);
